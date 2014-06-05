50c30db4b9d95962fd2aadb69ef9163cd0ecf3b4
Adam Kopeć <adam.kopec@gmail.com>
2014-06-05 15:59:27 +0200

    missing exceptions

---
+++ application/src/Application/Product/Exception/ProductPersistenceException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: adamkopec
+ * Date: 05.06.2014
+ * Time: 15:31
+ */
+
+namespace Application\Product\Exception;
+
+
+class ProductPersistenceException extends \Exception {
+
+} 
